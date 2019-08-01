d86644e079e3acb7357855adcaa0bb202a104cb5
SandhyaBridgelabz <sandhyapatil364@gmail.com>
2019-08-01 11:52:07 +0530

    Program 2 added and utility modified

---
+++ src/com/bridgelabz/functional/prog2.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.functional;
+
+public class prog2 {
+
+	public static void main(String[] args) {
+		System.out.println("this is program 2");
+	}
+
+}
--- src/com/bridgelabz/utility/utility.java
+++ src/com/bridgelabz/utility/utility.java
@@ -1,6 +1,8 @@
 package com.bridgelabz.utility;
 
+import java.util.Scanner;
+
 public class utility {
 	
-
+	Scanner scanner = new Scanner(System.in);
 }
